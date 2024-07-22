3c1fe88b238b0edebe529d2732cbbd861ba1904d
Olivier <herculepoirot75@hotmail.fr>
2024-07-22 13:33:48 +0200

    tests JS...

--- assets/script.js
+++ assets/script.js
@@ -18,16 +18,52 @@ const slides = [
   },
 ];
 
-const LeftArrowClick = document.getElementById("flecheDeGauche");
+/* 
 
-LeftArrowClick.addEventListener("click", function () {
-  alert("test de click");
-});
+f1: mise en place d'une fonction nombreDePhotos pour les 4 photos
+au chargement de la page
 
-const RightArrowClick = document.getElementById("flecheDeDroite");
+f2: mise en place d'une fonction pour indiquer la position initiale
+de la photo au chargement de la page pour ensuite décaler vers
+la droite ou vers la gauche la liste des 4 photos
 
-RightArrowClick.addEventListener("click", function () {
-  alert("2e test !");
-});
+f3 et f4: récupération de la banner et des 2 flèches
 
-console.log(slides);
+f5: modification de la largeur de la banner pour pouvoir y insérer
+les 4 photos
+
+f6 creation d'un for
+
+f7: création d'une div via JS intégrant les 4 images
+
+*/
+document.body.onload = function () {
+  nombreDePhotos = 4; /*f1*/
+  positionPhoto = 0; /*f2*/
+  banner = document.getElementById("banner"); /*f3*/
+  flecheDeGauche = document.getElementById("flecheDeGauche"); /*f4*/
+  flecheDeDroite = document.getElementById("flecheDeDroite"); /*f4*/
+};
+
+flecheDeGauche.addEventListener("click", () => {
+  positionPhoto.style.transform = translate(positionPhoto - 1 + 4)%4;)
+
+flecheDeDroite.addEventListener("click", () => {
+  positionPhoto=(positionPhoto + slides);
+  banner.style.transform = translateX(400+"px");});
+
+
+
+// const LeftArrowClick = document.getElementById("flecheDeGauche");
+
+// LeftArrowClick.addEventListener("click", function () {
+//   alert("test de click");
+// });
+
+// const RightArrowClick = document.getElementById("flecheDeDroite");
+
+// RightArrowClick.addEventListener("click", function () {
+//   alert("2e test !");
+// });
+
+// console.log(slides);
