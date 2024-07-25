997780c8a1ccb4a0882a6df43148c5749c2c76fc
Olivier <herculepoirot75@hotmail.fr>
2024-07-25 09:58:39 +0200

    encore presque ...

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,82 @@
 const slides = [
   {
-    name: "slide1",
+    name: "slide",
     image: "slide1.jpg",
     url: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    name: "slide2 active",
+    name: "slide active",
     image: "slide2.jpg",
     url: "./assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    name: "slide3",
+    name: "slide",
     image: "slide3.jpg",
     url: "./assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    name: "slide4",
+    name: "slide",
     image: "slide4.png",
     url: "./assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-
+//-------------------------------------------------------------------
 const flecheDeGauche = document.getElementById("flecheDeGauche");
 const flecheDeDroite = document.getElementById("flecheDeDroite");
 const banner = document.querySelector("#banner");
 const bannerImg = document.querySelectorAll(".banner-img");
-
+//-------------------------------------------------------------------
+// slides.forEach(("./assets/images/slideshow/slide") => {
+//   const slide = document.createElement("slide");
+//   slide.src = "./assets/images/slideshow/";
+//   const td = document.createElement("td");
+//   td.appendChild([...slide]);
+//   const tr =
+//     banner.querySelector("tr") ||
+//     banner.appendChild(document.createElement("tr"));
+//   tr.appendChild(td);
+// });
+//-------------------------------------------------------------------
 flecheDeGauche.addEventListener("click", (e) => {
-  const calcSlideImgPrecedente = e.target.id === "flecheDeGauche" ? 1 : -1;
-  const slideActive = document.querySelector(".active");
+  let calcSlideImgPrecedente;
+  if (e.target.id === "flecheDeGauche") {
+    calcSlideImgPrecedente = 1;
+  } else {
+    calcSlideImgPrecedente = -1;
+  }
 
-  let nouvelIndexFlecheGauche =
+  let slideActive = document.querySelector(".active");
+
+  let nouvelIndexFlecheDeGauche =
     calcSlideImgPrecedente + [...slides].indexOf(slideActive);
 
-  if (nouvelIndexFlecheGauche < 0)
-    nouvelIndexFlecheGauche = [...slides].length - 1;
-  slides[nouvelIndexFlecheGauche].classList.add("active");
+  if (nouvelIndexFlecheDeGauche < 0)
+    nouvelIndexFlecheDeGauche = [...slides].length - 1;
+  // slides[nouvelIndexFlecheDeGauche].classList.add("active");
   slideActive.classList.remove("active");
 });
-
+//-------------------------------------------------------------------
 flecheDeDroite.addEventListener("click", (a) => {
-  const calcSlideImgSuivante = a.target.id === "flecheDeDroite" ? 1 : -1;
-  const slideActive = document.querySelector(".active");
+  let calcSlideImgSuivante;
+  if (a.target.id === "flecheDeDroite") {
+    calcSlideImgSuivante = 1;
+  } else {
+    calcSlideImgSuivante = -1;
+  }
 
-  let nouvelIndexFlecheDroite =
+  let slideActive = document.querySelector(".active");
+
+  let nouvelIndexFlecheDeDroite =
     calcSlideImgSuivante - [...slides].indexOf(slideActive);
 
-  if (nouvelIndexFlecheDroite >= [...slides].length)
-    nouvelIndexFlecheDroite = 0;
-  slides[nouvelIndexFlecheDroite].classList.add("active");
+  if (nouvelIndexFlecheDeDroite >= [...slides].length)
+    nouvelIndexFlecheDeDroite = 0;
+  // slides[nouvelIndexFlecheDeDroite].classList.add("active");
   slideActive.classList.remove("active");
 });
-
-// flecheDeDroite.addEventListener("click", function () {
-//   alert("ola!");
-// });
-
-// flecheDeDroite.addEventListener("click", function () {
-//   for (let i = 0; i > slides.url; i++) {
-//     slides(slides.url[i]);
-//   }
-// });
-
-// const LeftArrowClick = document.getElementById("flecheDeGauche");
-
-// LeftArrowClick.addEventListener("click", function () {
-//   alert("test de click");
-// });
-
-// const RightArrowClick = document.getElementById("flecheDeDroite");
-
-// RightArrowClick.addEventListener("click", function () {
-//   alert("2e test !");
-// });
-
-// console.log(slides);
+//-------------------------------------------------------------------
