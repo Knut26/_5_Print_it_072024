b6d2510abf2a582b13a2660dda86ed79a971ffd1
Olivier <herculepoirot75@hotmail.fr>
2024-08-14 08:02:41 +0200

    DOMContentLoaded

--- assets/script.js
+++ assets/script.js
@@ -1,93 +1,126 @@
-const slides = [
-  {
-    name: "slide",
-    image: "slide1.jpg",
-    url: "./assets/images/slideshow/slide1.jpg",
-    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-  },
-  {
-    name: "slide",
-    image: "slide2.jpg",
-    url: "./assets/images/slideshow/slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  },
-  {
-    name: "slide",
-    image: "slide3.jpg",
-    url: "./assets/images/slideshow/slide3.jpg",
-    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  },
-  {
-    name: "slide",
-    image: "slide4.png",
-    url: "./assets/images/slideshow/slide4.png",
-    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = [
+    {
+      name: "slide",
+      image: "slide1.jpg",
+      url: "./assets/images/slideshow/slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+      name: "slide",
+      image: "slide2.jpg",
+      url: "./assets/images/slideshow/slide2.jpg",
+      tagLine:
+        "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+      name: "slide",
+      image: "slide3.jpg",
+      url: "./assets/images/slideshow/slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+      name: "slide",
+      image: "slide4.png",
+      url: "./assets/images/slideshow/slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+  ];
 
-//-------------------------------------------------------------------
+  //-------------------------------------------------------------------
+  const bannerImg = document.getElementById("banner-img");
+  const flecheDeGauche = document.getElementById("flecheDeGauche");
+  const flecheDeDroite = document.getElementById("flecheDeDroite");
+  const dots = document.getElementById("dots");
+  let currentIndex = 0;
+  // console.log(dots);
+  // console.log(slides[1].url);
 
-const flecheDeGauche = document.getElementById("flecheDeGauche");
-const flecheDeDroite = document.getElementById("flecheDeDroite");
-const banner = document.getElementById("banner");
-const dots = document.getElementById("dots");
-let currentIndex = 0;
-// console.log(dots);
-// console.log(slides[1].url);
-
-slides.forEach((slide) => {
-  const dot = document.createElement("span");
-  dot.classList.add("dot");
-  dots.appendChild(dot);
-});
-
-function createImage(index) {
-  const img = document.createElement("img");
-  const p = document.createElement("p");
-  img.classList.add("banner-img");
-  img.classList.add("active");
-  img.setAttribute("src", slides[index].url);
-  p.innerHTML = slides[index].tagLine;
-  banner.appendChild(p);
-  banner.appendChild(img);
-}
-
-createImage(0);
-
-flecheDeDroite.addEventListener("click", (e) => {
-  e.preventDefault();
-  createImage(1);
-  flecheDeDroite.addEventListener("click", (e) => {
-    e.preventDefault();
-    createImage(2);
-    flecheDeDroite.addEventListener("click", (e) => {
-      e.preventDefault();
-      createImage(3);
-      flecheDeDroite.addEventListener("click", (e) => {
-        e.preventDefault();
-        createImage(0);
-      });
+  function updateDots(index) {
+    dots.forEach((dot, i) => {
+      if (i === index) {
+        dot.classList.add("dot_selected");
+      } else {
+        dot.classList.remove("dot_selected");
+      }
     });
+  }
+
+  function updateImage(index, direction) {
+    if (currentIndex === -1 && direction === "left") {
+      currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === "right") {
+      currentIndex = 0;
+    }
+  }
+
+  let imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+  bannerImg.src = imagePath;
+  bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+  let tagLine = slides[currentIndex].tagLine;
+  document.querySelector("p").innerHTML = tagLine;
+  console.log(`clique sur ${direction}`);
+
+  flecheDeGauche.addEventListener("click", function () {
+    currentIndex = currentIndex - 1;
+    updateImage(currentIndex, "left");
+    updateDots(currentIndex);
   });
 });
 
-flecheDeGauche.addEventListener("click", (e) => {
-  e.preventDefault();
-  createImage(3);
-  flecheDeGauche.addEventListener("click", (e) => {
-    e.preventDefault();
-    createImage(2);
-    flecheDeGauche.addEventListener("click", (e) => {
-      e.preventDefault();
-      createImage(1);
-      flecheDeGauche.addEventListener("click", (e) => {
-        e.preventDefault();
-        createImage(0);
-      });
-    });
-  });
-});
+// slides.forEach((slide) => {
+//   const dot = document.createElement("span");
+//   dot.classList.add("dot");
+//   dots.appendChild(dot);
+// });
+
+// function createImage(index) {
+//   const img = document.createElement("img");
+//   const p = document.createElement("p");
+//   img.classList.add("banner-img");
+//   img.classList.add("active");
+//   img.setAttribute("src", slides[index].url);
+//   p.innerHTML = slides[index].tagLine;
+//   banner.appendChild(p);
+//   banner.appendChild(img);
+// }
+
+// createImage(0);
+
+// flecheDeDroite.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   createImage(1);
+//   flecheDeDroite.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     createImage(2);
+//     flecheDeDroite.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       createImage(3);
+//       flecheDeDroite.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         createImage(0);
+//       });
+//     });
+//   });
+// });
+
+// flecheDeGauche.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   createImage(3);
+//   flecheDeGauche.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     createImage(2);
+//     flecheDeGauche.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       createImage(1);
+//       flecheDeGauche.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         createImage(0);
+//       });
+//     });
+//   });
+// });
 
 // //--------ajout
 // function UpdateDots(index) {
