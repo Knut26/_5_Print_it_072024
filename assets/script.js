66607c005352c8efe8d994d047f19bd407ac6e6e
Olivier <herculepoirot75@hotmail.fr>
2024-08-27 16:03:46 +0200

    manque les 2 dernières slides et fleche left

--- assets/script.js
+++ assets/script.js
@@ -1,113 +1,170 @@
-const slides = [
-  {
-    name: "slide active",
-    name: "slide",
-    image: "slide1.jpg",
-    url: "./assets/images/slideshow/slide1.jpg",
-    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-  },
-  {
-    name: "slide",
-    image: "slide2.jpg",
-    url: "./assets/images/slideshow/slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  },
-  {
-    name: "slide",
-    image: "slide3.jpg",
-    url: "./assets/images/slideshow/slide3.jpg",
-    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  },
-  {
-    name: "slide",
-    image: "slide4.png",
-    url: "./assets/images/slideshow/slide4.png",
-    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  },
-];
-//-------------------------------------------------------------------
-const banner = document.getElementById("banner");
-const buttons = document.querySelectorAll(".btn");
-const flecheDeGauche = document.getElementById("flecheDeGauche");
-const flecheDeDroite = document.getElementById("flecheDeDroite");
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = [
+    {
+      name: "slide1 active",
+      image: "slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+      name: "slide2",
+      image: "slide2.jpg",
+      tagLine:
+        "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+      name: "slide3",
+      image: "slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+      name: "slide4",
+      image: "slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+  ];
 
-const slide1 = document.createElement("img");
-slide1.classList.add("active");
-slide1.src = "./assets/images/slideshow/slide1.jpg";
-banner.appendChild(slide1);
-const tagLine1 = document.createElement("div");
-tagLine1.innerText = "Impressions tous formats ";
-banner.appendChild(tagLine1);
-const span1 = document.createElement("span");
-span1.innerText = "boutique en ligne";
-banner.appendChild(span1);
+  let bannerImg = document.getElementById("banner-img"); // récupère l'élément de l'image de la banner par son Id
+  let tagline = document.getElementById("tagline"); // récupère l'élément du texte de la bannière par son Id
+  const right = document.getElementById("arrow-right"); // récupère l'élément de la flèche de droite par son Id
+  const left = document.getElementById("arrow-left"); // récupère l'élément de la flèche de gauche par son Id
+  let dots = document.querySelectorAll(".dot"); // récupère l'ensemble des éléments représentant les dots
+  const url = "./assets/images/slideshow/"; // définit le chemin de base pour les images
+  const buttons = document.querySelectorAll(".button");
 
-const slide2 = document.createElement("img");
-slide1.src = "./assets/images/slideshow/slide2.jpg";
-banner.appendChild(slide2);
-const tagLine2 = document.createElement("div");
-tagLine2.innerText = "Tirages haute définition grand format ";
-banner.appendChild(tagLine2);
-const span2 = document.createElement("span");
-span2.innerText = "pour vos bureaux et events";
-banner.appendChild(span2);
+  function start(slide) {
+    bannerImg.src = url + slides[slide].image; // màj de la source de l'image de la banner
+    tagline.innerHTML = slides[slide].tagLine; // màj du texte de la banner
+    dots.forEach((dot, i) => {
+      if (i === slide) {
+        dot.classList.add("dot_selected"); // ajoute la classe "dot_selected" au dot correspondant au slide affiché
+      } else {
+        dot.classList.remove("dot_selected"); // retire la classe "dot_selected" au dot du slide non affiché
+      }
+    });
+  }
 
-const slide3 = document.createElement("img");
-slide1.src = "./assets/images/slideshow/slide3.jpg";
-banner.appendChild(slide3);
-const tagLine3 = document.createElement("div");
-tagLine3.innerText = "Grand choix de couleurs ";
-banner.appendChild(tagLine3);
-const span3 = document.createElement("span");
-span3.innerText = "de CMJN aux pantones";
-banner.appendChild(span3);
+  start(0); // initialise le carrousel avec la première diapositive
 
-const slide4 = document.createElement("img");
-slide1.src = "./assets/images/slideshow/slide4.png";
-banner.appendChild(slide4);
-const tagLine4 = document.createElement("div");
-tagLine4.innerText = "Autocollants ";
-banner.appendChild(tagLine4);
-const span4 = document.createElement("span");
-span4.innerText = "avec découpe laser sur mesure";
-banner.appendChild(span4);
+  let currentIndex = 0; // création de la variable pour la slide de base
 
-const slideAll = [slide1, slide2, slide3, slide4];
+  buttons.forEach((button) => {
+    for (let i = 0; i <= [...slides].length; i++) {
+      button.addEventListener("click", (e) => {
+        let nextSlide = e.target.id === "arrow-right" ? +1 : -1;
+        bannerImg.src = url + slides[nextSlide].image;
+        tagline.innerHTML = slides[nextSlide].tagLine;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const calcNextSlide = e.target.id === "flecheDeDroite" ? 1 : -1;
-    const slideActive = document.querySelector(".active");
-    newIndex = calcNextSlide + [...slides].indexOf(slideActive);
-    if (newIndex < 0) newIndex = [...slides].length - 1;
-    if (newIndex > [...slides].length) newIndex = 0;
-    slideAll[newIndex].classList.add("active");
-    slideActive.classList.remove("active");
-    console.log(calcNextSlide);
-    console.log(newIndex);
+        if (i > [...slides].length) {
+          i = 0;
+        }
+
+        if (i < [...slides].length) {
+          i = [...slides].length - 1;
+        }
+
+        if (i <= [...slides].length) {
+          dots[nextSlide].classList.add("dot_selected");
+          dots[currentIndex].classList.remove("dot_selected");
+        }
+      });
+    }
   });
 });
 
-// creation d'autant de dots qu'il y a d'images
-function createDots() {
-  const dots = document.getElementById("dots");
-  for (let i = 0; i < slides.length; i++) {
-    const dot = document.createElement("div");
-    dot.setAttribute("class", "dot");
-    dots.appendChild(dot);
+//      console.log(nextSlide, [...slides].length); => 1 array
+//      console.log(nextSlide, [...slides]); => 1 array de 4 données
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+//
+//
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  const slides = [
+    {
+      name: "slide1 active",
+      image: "slide1.jpg",
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+      name: "slide2",
+      image: "slide2.jpg",
+      tagLine:
+        "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+      name: "slide3",
+      image: "slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+      name: "slide4",
+      image: "slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+  ];
+
+  let bannerImg = document.getElementById("banner-img"); // récupère l'élément de l'image de la banner par son Id
+  let tagline = document.getElementById("tagline"); // récupère l'élément du texte de la bannière par son Id
+  const right = document.getElementById("arrow-right"); // récupère l'élément de la flèche de droite par son Id
+  const left = document.getElementById("arrow-left"); // récupère l'élément de la flèche de gauche par son Id
+  let dots = document.querySelectorAll(".dot"); // récupère l'ensemble des éléments représentant les dots
+  const url = "./assets/images/slideshow/"; // définit le chemin de base pour les images
+  const buttons = document.querySelectorAll(".button");
+
+  function start(slide) {
+    bannerImg.src = url + slides[slide].image; // màj de la source de l'image de la banner
+    tagline.innerHTML = slides[slide].tagLine; // màj du texte de la banner
+    dots.forEach((dot, i) => {
+      if (i === slide) {
+        dot.classList.add("dot_selected"); // ajoute la classe "dot_selected" au dot correspondant au slide affiché
+      } else {
+        dot.classList.remove("dot_selected"); // retire la classe "dot_selected" au dot du slide non affiché
+      }
+    });
   }
-}
-createDots();
-// // ajoute ou enleve le dot_selected sur le dot
-// function updateDots() {
-//   const dotsList = document.querySelectorAll(".dot");
-//   for (let i = 0; i < dotsList.length; i++) {
-//     const dot = dotsList[index];
-//     if (index == position) {
-//       dot.classList.add("dot_selected");
-//     } else {
-//       dot.classList.remove("dot_selected");
-//     }
-//   }
-// }
+
+  start(0); // initialise le carrousel avec la première diapositive
+
+  let currentIndex = 0; // création de la variable pour la slide de base
+
+  left.addEventListener("click", () => {
+    currentIndex--; // décrémente la variable de -1 pour aller à la diapositive précédente
+    if (currentIndex < 0) {
+      currentIndex = slides.length - 1; // on revient à la dernière slide du tableau si on décrémente de -1 à la première slide
+    }
+    bannerImg.src = url + slides[currentIndex].image; // màj de l'image de la bannière en fonction de la slide
+    tagline.innerHTML = slides[currentIndex].tagLine; // màj du texte de la bannière en fonction de la slide
+    dots.forEach((dot) => {
+      dot.classList.remove("dot_selected"); // retire la classe "dot_selected" des dots
+    });
+    dots[currentIndex].classList.add("dot_selected"); // ajoute la classe "dot_selected" au dot correspondant au slide affiché
+    // bannerImg.style.transition = "all 500ms ease";
+  });
+
+  right.addEventListener("click", () => {
+    currentIndex++; // incrémente la variable de 1 pour passer à la diapositive suivante
+    if (currentIndex >= slides.length) {
+      currentIndex = 0; // si la variable est égale ou > au tableau on repasse à la 1ère slide
+    }
+    bannerImg.src = url + slides[currentIndex].image; // màj de l'image de la bannière en fonction de la slide
+    tagline.innerHTML = slides[currentIndex].tagLine; // màj du texte de la bannière en fonction de la slide
+    dots.forEach((dot) => {
+      dot.classList.remove("dot_selected"); // retire la classe "dot_selected" des dots
+    });
+    dots[currentIndex].classList.add("dot_selected"); // ajoute la classe "dot_selected" au dot correspondant au slide affiché
+    // bannerImg.style.transition = "all 500ms ease";
+  });
+});
+
+ */
